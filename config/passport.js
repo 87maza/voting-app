c9f109450baef83c85ec7fb1ff04935db3bed58c
87maza <87maza@gmail.com>
2016-04-28 20:19:01 -0700

    deleted the local obj for auth

--- config/passport.js
+++ config/passport.js
@@ -42,7 +42,7 @@ module.exports = function(passport) {
 
                 // find a user whose email is the same as the forms email
                 // we are checking to see if the user trying to login already exists
-                User.findOne({ 'local.email' :  email }, function(err, user) {
+                User.findOne({ 'email' :  email }, function(err, user) {
                     // if there are any errors, return the error
                     if (err)
                         return done(err);
@@ -55,10 +55,9 @@ module.exports = function(passport) {
                         // if there is no user with that email
                         // create the user
                         var newUser = new User();
-                        console.log("email is: ");
                         // set the user's local credentials
-                        newUser.local.email = email;
-                        newUser.local.password = newUser.generateHash(password);
+                        newUser.email = email;
+                        newUser.password = newUser.generateHash(password);
 
                         // save the user
                         newUser.save(function(err) {
@@ -89,7 +88,7 @@ module.exports = function(passport) {
 
             // find a user whose email is the same as the forms email
             // we are checking to see if the user trying to login already exists
-            User.findOne({'local.email': email}, function (err, user) {
+            User.findOne({'email': email}, function (err, user) {
                 // if there are any errors, return the error before anything else
                 if (err)
                     return done(err);

2975391c4214f81755b68cd7e3cfd73440b3230d
87maza <87maza@gmail.com>
2016-05-05 21:36:42 -0700

    first commit

--- app/models/poll.js
+++ app/models/poll.js
@@ -5,8 +5,7 @@ var pollSchema = new Schema({
     pollId: String,
     created_at: Date,
     question:  String,
-    author: String,
-    body: String,
+    author: String, //nest object with a username query,
     addField: String,
     options: Array,
     pollResults: Array,
@@ -14,3 +13,12 @@ var pollSchema = new Schema({
 });
 
 module.exports = mongoose.model('Poll', pollSchema);
+
+//get poll data for all polls and indiv poll id's
+//post new poll data
+//put modify options of the survey by adding new fields/choices
+//delete poll data if user created the poll
+//1 user can have many polls
+
+//route polls with /:id params
+
